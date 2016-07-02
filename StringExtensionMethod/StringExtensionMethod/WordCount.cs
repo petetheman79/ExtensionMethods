0525f171e1a5b67c981c4e3dc4d8f31f11e2b8da
Peter <peter@nextsteps.co.za>
2016-07-02 16:23:05 +0200

    initial commit

---
+++ StringExtensionMethod/StringExtensionMethod/WordCount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensionMethod
+{
+    public static class Utils
+    {
+        public static int WordCount(this string str)
+        {
+            return str.Split(' ').Count();
+        }
+    }
+}
---
+++ StringExtensionMethod/StringExtensionTests/Test1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using StringExtensionMethod;
+
+namespace StringExtensionTests
+{
+    [TestFixture]
+    public class Test1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var sentence = "Mary had a little lamb";
+            Assert.AreEqual(5, sentence.WordCount());
+        }
+    }
+}
